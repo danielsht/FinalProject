1eae942122fe72a406a45962e0fd348f895ac88b
Daniel Shteremberg <dshteremberg@smu.edu>
2016-04-27 16:37:26 -0500

    arcs are now growing and initial waveform code is commented
    arc length is now growing instead of moving and the waveform code is up
    but needs some work so its commented out in this version

--- P5/sketch.js
+++ P5/sketch.js
@@ -2,38 +2,52 @@ var bytes = [];
 var arcProp = {};
 var mySound;
 var sound = false;
-var songDuration;
-var intervalChange;
-var fileSize;
-var byteCounter;
+var songDuration, intervalChange, changeOnFrame, fileSize, byteCounter;
 
 function setup() {
-	createCanvas(640, 480); //make panel to draw on in the site
+    createCanvas(640, 480); //make panel to draw on in the site
     stroke('red'); //change line color to red 
     noFill(); //doesnt allow arcs to fill to center like a pie chart thus covering other arcs
     byteCounter = 10000;
     frameRate();
+    changeOnFrame = 60;
 }
 
 function draw() {
     background(0);
     for (var i = 0; i < bytes.length; i++) { //cycle through bites and create the arc using the object properites
-        arc(width/2, height/2, bytes[i].HAndW, bytes[i].HAndW, bytes[i].start, bytes[i].stop);
+        arc(width/2, height/2, bytes[i].HAndW, bytes[i].HAndW, bytes[i].start, animateArcs(bytes[i]));
     }
-    animateArcs();
-    if(frameCount % 10 == 9 && sound){
-        readBlob(byteCounter, byteCounter += 10000)
+    if(frameCount % changeOnFrame == (changeOnFrame - 1) && sound){
+        readBlob(byteCounter, byteCounter += intervalChange);
+        if(mySound.currentTime() == 0){
+            sound = false;
+        }
     }
+    //uncomment for waveform not complete yet
+    // if(sound){
+    //     var waveform = mySound.getPeaks();
+    //     fill(0, 0, 255);
+    //     stroke(0);
+    //     strokeWeight(1);
+    //     beginShape();
+    //     for (var i = 0; i< waveform.length; i++){
+    //         vertex(map(i, 0, waveform.length, 0, width), map(waveform[i], -1, 1, height, 0));
+    //     }
+    //     endShape();
+
+    //     drawCursor();
+    // }
 }
 
 function mapValues(){ //map byte values to degrees and add to the arc properties then return the array with the arc properties
-	var mappedValues = [];
-	for (var i = 0; i < bytes.length; i++) {
-		var m = map(int(bytes[i]), 00000000, 99999999, 0, 360);
-		mappedValues[i] = m;
+    var mappedValues = [];
+    for (var i = 0; i < bytes.length; i++) {
+        var m = map(bytes[i], 0, 9999999999, 0, 360);
+        mappedValues[i] = m;
         mappedValues[i] = arcArray(mappedValues[i]);
-	}
-	return mappedValues;
+    }
+    return mappedValues;
 }
 
 function newFileRead(){
@@ -71,7 +85,7 @@ function readBlob(opt_startByte, opt_stopByte, files) { //read file byte by byte
         var placemark = 0, dv = new DataView(this.result), limit = dv.byteLength - 4, output;
         while( placemark <= limit ){
             output = dv.getUint32(placemark);  
-            bytes[i] = output.toString(16);
+            bytes[i] = output;
             placemark += 4;
             i++;
         }
@@ -88,27 +102,33 @@ function arcArray(bytesToBePlaced){ //get properties for the arc and return them
     if(arcStart + bytesToBePlaced >= 360){ //convert to radians and account set any stop values 361+ to 0+
         arcStop = (arcStart - 360) + bytesToBePlaced;
         arcStart = arcStart * (PI/180);
-        arcStop = arcStart * (PI/180);
+        arcStop = arcStop * (PI/180);
     } else { //just convert to radians and set arc stop
         arcStop = arcStart + bytesToBePlaced;
         arcStart = arcStart * (PI/180);
         arcStop = arcStop * (PI/180);
     }
     
-    return arcProp = {HAndW:arcHeightAndWidth, start:arcStart, stop:arcStop} //return arc object properties
+    return arcProp = {HAndW:arcHeightAndWidth, start:arcStart, stop:arcStop, currentLength:arcStart} //return arc object properties
 }
 
-function animateArcs(){
-    for (var i = 0; i < bytes.length; i++) {
-        bytes[i].start += (1 * (PI/180));
-        bytes[i].stop += (1 * (PI/180));
+function animateArcs(maxSize){
+    if(maxSize.currentLength <= maxSize.stop){
+        maxSize.currentLength += ((maxSize.stop/changeOnFrame) * (PI/180));
     }
+    return maxSize.currentLength;
 }
 
 function playMusic(){
     mySound.setVolume(1.0);
     mySound.play();
     songDuration = mySound.duration();
-    intervalChange = (fileSize / (songDuration * 1000)) + 1000;
+    intervalChange = fileSize / songDuration;
     sound = true;
+}
+
+function drawCursor() {
+  noStroke();
+  fill(0,255,0);
+  rect(map(mySound.currentTime(), 0, mySound.duration(), 0, width), 0, 5, height);
 }
